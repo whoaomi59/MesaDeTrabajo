fe4a7e366d8f1a122bf39cdbe085793d77207cac
Carlos Andres Cuellar <caacuellar@sena.edu.co>
2025-03-26 10:14:36 -0500

    mejoras pagina

--- src/components/api/whassapp.jsx
+++ src/components/api/whassapp.jsx
@@ -4,18 +4,27 @@ export const WhatsAppLink = ({ phoneNumber, message }) => {
   // Elimina espacios y caracteres no numéricos del número
   const cleanedNumber = `57${phoneNumber.replace(/\D/g, "")}`; // Prefijo +57 para Colombia
   const encodedMessage = encodeURIComponent(message);
-  /*  const whatsappUrl = `https://wa.me/${cleanedNumber}?text=${encodedMessage}`; */
+  const whatsappUrlMovil = `https://wa.me/${cleanedNumber}?text=${encodedMessage}`;
   const whatsappUrl = `https://web.whatsapp.com/send/?phone=${cleanedNumber}&text=${encodedMessage}`;
 
   return (
-    <a
-      href={whatsappUrl}
-      target="_blank"
-      rel="noopener noreferrer"
-      className="text-green-700 hover:underline flex"
-    >
-      <ChatBubbleBottomCenterTextIcon className="w-5 mr-1.5 text-gray-800" />
-      {phoneNumber}
-    </a>
+    <div>
+      <a
+        href={whatsappUrl}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-blue-500 hover:underline flex"
+      >
+        whassapp Web {phoneNumber}
+      </a>{" "}
+      <a
+        href={whatsappUrlMovil}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-green-600 hover:underline"
+      >
+        whassapp Movil {phoneNumber}
+      </a>
+    </div>
   );
 };
